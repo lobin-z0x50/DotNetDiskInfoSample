71165dded4276a8383c9292f8cd7d5840df18f6d
Wataru Maeda <w.maeda@neogenia.co.jp>
2015-12-07 17:51:37 +0900

    Initial commit.

---
+++ Sample1/DiskInfoCollector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace Sample1
+{
+    /// <summary>
+    /// ディスク情報を収集するクラス
+    /// </summary>
+    public class DiskInfoCollector
+    {
+        /// <summary>
+        /// 収集結果を格納しているDictionary
+        /// キーはデバイスパス
+        /// </summary>
+        public IDictionary<string, IDictionary<string, string>> Result = new Dictionary<string, IDictionary<string, string>>();
+
+
+        /// <summary>
+        /// hdCollection に指定のキーがあればその値を返し、なければ新規Dictionaryを登録して返す
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private IDictionary<string, string> _GetOrCreateDictionary(string key)
+        {
+            if (Result.ContainsKey(key))
+            {
+                return Result[key];
+            }
+            var dic = new Dictionary<string, string>();
+            Result[key] = dic;
+            return dic;
+        }
+
+        private void _Collect(string tableName, string keyName)
+        {
+            var searcher = new ManagementObjectSearcher($"SELECT * FROM {tableName}");
+
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                string key = obj[keyName].ToString();
+                var dic = _GetOrCreateDictionary(key);
+                foreach (var kv in obj.Properties) { dic[kv.Name] = kv.Value?.ToString(); }
+            }
+        }
+
+        /// <summary>
+        /// Win32_DiskDriveより情報収集
+        /// </summary>
+        public void CollectDiskDrive() { _Collect("Win32_DiskDrive", "DeviceId"); }
+
+        /// <summary>
+        /// Win32_PhysicalMediaより情報収集
+        /// </summary>
+        public void CollectPhysicalMedia() { _Collect("Win32_PhysicalMedia", "Tag"); }
+    }
+}
---
+++ Sample1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collector = new DiskInfoCollector();
+
+            collector.CollectDiskDrive();        // Win32_DiskDrive
+            collector.CollectPhysicalMedia();    // Win32_PhysicalMedia
+
+            // 結果を出力
+            foreach (var deviceId in collector.Result.Keys)
+            {
+                System.Console.WriteLine($"--- {deviceId} -----");
+
+                var hdi = collector.Result[deviceId];
+                foreach (var key in hdi.Keys)
+                {
+                    System.Console.WriteLine($"{key}: {hdi[key]}");
+                }
+
+            }
+
+            Console.WriteLine();
+
+
+            // 物理ディスクのみ抽出
+            var physicalDrives = collector.Result.Keys.Where(key => key.Contains("PHYSICALDRIVE"));
+
+            // 1つ目の物理ディスクドライブのシリアル番号
+            if (physicalDrives.Any())
+            {
+                Console.WriteLine("First physical drive = " + physicalDrives.First());
+                var dic = collector.Result[physicalDrives.First()];
+                Console.WriteLine("Serial Number = " + dic["SerialNumber"]);
+            }
+            else
+            {
+                Console.WriteLine("No physical drive found!");
+            }
+
+            // wait
+            Console.ReadLine();
+        }
+    }
+}
